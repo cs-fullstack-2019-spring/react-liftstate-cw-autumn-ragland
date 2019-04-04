3d416b91d9e8d59b903c153c1c481fa233964362
autumn-ragland <raglanda998@gmail.com>
2019-04-04 16:01:25 -0500

    broken bonus

--- lift_state_cw/src/App.js
+++ lift_state_cw/src/App.js
@@ -7,23 +7,45 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state={
-      clickedLast:''
+      clickedLast:'',
+      winner:''
     }
   }
   //change state of variable clicked last
   whoClickedLast =(playerClicked) =>{
-    this.setState({clickedLast:playerClicked})
+
+    // this.setState({clickedLast:playerClicked});
+
+    console.log(playerClicked);
+
+    let scoreOne = 0;
+    let scoreTwo = 0;
+
+    if(playerClicked === 1){
+      scoreOne += 1;
+    }
+    else if(playerClicked === 2){
+      scoreTwo += 1;
+    }
+
+    if(scoreOne > scoreTwo){
+      this.setState({winner:1});
+    }
+    else if(scoreTwo > scoreOne){
+      this.setState({winner:2});
+    }
   };
 
   render() {
 
     return (
       <div className="App">
-        {/*Display who clicked last*/}
-        <h1>Player {this.state.clickedLast} Clicked Last</h1>
         {/*render player components with player name and clicked last function*/}
-        <Player player={1} whoClickedLast={this.whoClickedLast}/>
-        <Player player={2} whoClickedLast={this.whoClickedLast}/>
+        <div className={'playerGrid'}>
+          <Player player={1} whoClickedLast={this.whoClickedLast}/>
+          <h1>Winner: Player {this.state.winner}</h1>
+          <Player player={2} whoClickedLast={this.whoClickedLast}/>
+        </div>
 
       </div>
     );
--- lift_state_cw/src/Player.js
+++ lift_state_cw/src/Player.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 
 class Player extends Component {
-
+    //set variable score with state
+    state={
+        score:0
+    };
     //run who clicked last function
     constructor(props) {
         super(props);
-        this.whoClickedLast()
+        this.whoClickedLast();
     }
     //determine who clicked last
     whoClickedLast = () => {
-        this.props.whoClickedLast(this.props.player)
+        this.props.whoClickedLast(this.props.player);
+        //add one to score of corresponding player
+        this.setState({score:this.state.score+1})
     };
 
     render() {
-
         return (
             <div>
                 {/*display player name and button*/}
-                <h3>Player {this.props.player}</h3>
+                <h2>Player {this.props.player}</h2>
+                <h3>Score:{this.state.score}</h3>
                 {/*when the button is clicked run who clicked last*/}
-                <button onClick={this.whoClickedLast}><h3>Click Me</h3></button>
+                <button onClick={this.whoClickedLast}><p>1pt</p></button>
             </div>
         );
     }
